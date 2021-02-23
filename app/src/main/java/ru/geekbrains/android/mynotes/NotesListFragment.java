0cd66daa1f4cbb9315414c7547c2f2b29d60726d
GeekBrainsStudent <kvazzz84@gmail.com>
2021-02-23 21:28:00 +0900

    6. update NotesListFragment

--- app/src/main/java/ru/geekbrains/android/mynotes/NotesListFragment.java
+++ app/src/main/java/ru/geekbrains/android/mynotes/NotesListFragment.java
@@ -1,9 +1,15 @@
 package ru.geekbrains.android.mynotes;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -27,7 +33,16 @@ public class NotesListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_notes, container, false);
+        View view = inflater.inflate(R.layout.fragment_list_notes, container, false);
+        setHasOptionsMenu(true);
+        Log.d("TAG", "onCreateView NotesListFragment");
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG", "onDestroy NotesListFragment");
     }
 
     @Override
@@ -41,6 +56,24 @@ public class NotesListFragment extends Fragment {
                 setOnClickListener(tvLayout, note);
                 root.addView(tvLayout);
             }
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_list_menu, menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.search :
+//                TODO поиск
+            case R.id.sort :
+//                TODO сортировка
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
@@ -76,7 +109,7 @@ public class NotesListFragment extends Fragment {
                     }
                 } else {
                     fm.beginTransaction()
-                            .replace(R.id.fragment_container, NoteFragment.newInstance(note))
+                            .replace(R.id.fragment_list, NoteFragment.newInstance(note))
                             .addToBackStack(null)
                             .commit();
                 }
